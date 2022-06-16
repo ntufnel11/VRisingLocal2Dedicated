88a521dc2c83b10ff1fbdf81bc6921ed42029f8c
ntufnel11 <49075456+ntufnel11@users.noreply.github.com>
2022-06-16 09:26:20 -0400

    art and choice conditional direction

--- vrising.bat
+++ vrising.bat
@@ -1,14 +1,65 @@
+@echo off
+echo                      i                    i:               
+echo                   .QB.                     QB:             
+echo                 iBBB                        BBQi           
+echo               iBBBB.  Pr                rb   BBBQ:         
+echo              QBQKBQ   QBBP            5BBB   5BPgBZ        
+echo            rBBZKSBq   BQBBBP        PBBBQB   LBXqXBB:      
+echo           UBZEq2PBB   JBEQBBRi:rii:QQBQZB2   BMPsP2DBr     
+echo          1BZZP21EXB5   BgI.  .v:rL.  :qEB   PBXduuPSgQr    
+echo         iBDbdIsIbU2Bb  .RY  .::77i:.  LQ.  QBIuZ5Y1PPQB.   
+echo         BQddKLJIEujIBB:iEMSrqREvjEQJ7XMP rBB5s2PbsL5PZBB   
+echo        uBDPPs7JXP1JUUQBBdKZQggMvjQZMQRSRBBQ5jUUdb27jIKgBr  
+echo        BBEd2Lru5dujss1MQBgQEgggDgDDMgBBBBQqsjuSPEIr7USdBB  
+echo        BMgPqPPPqdIjJ7vIMQBBq EMggRQ:2BBQRZjYv1SPPg5P5bdBB  
+echo        BBQBB5YBBMbbKgPXdZQBBIuXdKJISBBQEMbggPUdZBBJjQBBQB  
+echo        BQBJ    QQBBB5SBBZBBBBQPPSPgBBBBRBBuLMBBBB    rQBB. 
+echo        BB.      BB1   .BBgu2BBBQBBBBUvEBB:   iBB:      BB  
+echo        B.       Bv     S7    :BBBBr    .P     .B        B  
+echo                                KB                          
+                                                                                      
+Echo.
+Echo. 
+Echo Welcome to the VRising Local to Dedicated Server Migration Tool
+Echo. 
+Echo Checking Permissions....
+
+:CHECKPERMISSIONS
+@echo off
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    ECHO Administrator PRIVILEGES Detected! 
+    GOTO FIND_LOCALSAVES
+) ELSE (
+    ECHO .......
+    ECHO .......
+    ECHO NOT AN ADMIN! Please rerun the script as Administrator!
+    ECHO .......
+    ECHO .......
+    pause
+    exit
+)
+
 :FIND_LOCALSAVES
 @echo off
-CHOICE /N
-set /p LOCALSAVES="Local Saves Directory: "
-echo The directory %LOCALSAVES% contains your autosaves, is this correct?
+echo.
+echo.
+set /p LOCALSAVES="Please enter the Local VRising Directory that contains your autosaves (Default in Appdata): "
 echo ..
 echo ..
-CHOICE /M "Press Y if you see your autosaves in this location"
-set /p DEDICATEDSERVER="VRisingDedicatedServer folder path: "
-echo The directory %DEDICATEDSERVER% contains The VRisingServer.exe program, is this correct?
-CHOICE /M "Press Y for Yes if you can see the VRisingServer.exe program in this location"
+set /p DEDICATEDSERVER="VRisingDedicatedServer folder path containing VRisingServer.exe: "
+echo Saves Will be copied from: %LOCALSAVES%
+echo.
+echo to
+echo.
+echo %DATAPATH%
+CHOICE /M "Are these Paths correct?"
+IF %ERRORLEVEL% EQU 1 (
+    GOTO CHOOSENAMES
+) ELSE (
+    echo Please reselect the correct folders
+    GOTO FIND_LOCALSAVES
+)
 
 echo ..............
 echo ..............
@@ -38,13 +89,13 @@ echo set SteamAppId=1604030
 echo echo "Starting V Rising Dedicated Server - PRESS CTRL-C to exit"
 echo.
 echo @echo on
-echo VRisingServer.exe -persistentDataPath .\save-data -serverName "%SERVERNAME" -saveName "%SAVENAME" -logFile ".\logs\VRisingServer.log"
+echo VRisingServer.exe -persistentDataPath .\save-data -serverName "%SERVERNAME%" -saveName "%SAVENAME%" -logFile ".\logs\VRisingServer.log"
 )> %DEDICATEDSERVER%\Start_%SAVENAME%_Server.bat
 
 
 :HOSTJSON
 echo Backing up old ServerHostSettings.json file
-mv %DATAPATH%\ServerHostSettings.json ServerHostSettingsOLD.jsonbak
+move %DATAPATH%\ServerHostSettings.json ServerHostSettingsOLD.jsonbak
 echo Creating Dedicated Host json file
 (
 echo {
@@ -75,7 +126,7 @@ echo }
 :COPYGAMESETTINGS
 xcopy %DEDICATEDSERVER%
 echo copying ServerGameSettings.json file
-xcopy /c/y %DEDICATEDSERVER%\VRisingServer_Data\StreamingAssets\SettingsServerGameSettings.json %DATAPATH%\ServerGameSettings.json
+xcopy /c/y %DEDICATEDSERVER%\VRisingServer_Data\StreamingAssets\ServerGameSettings.json %DATAPATH%\ServerGameSettings.json
 
 echo Saves Copied Over Successfully
 
